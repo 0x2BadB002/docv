222fa72abbcc77a9682ed4908371f7e698488719
Kovalev Pavel <kovalev5690@gmail.com>
2025-08-23 14:49:40 +0300

    Add document

--- docv-pdf/src/document.rs
+++ docv-pdf/src/document.rs
@@ -1,8 +1,11 @@
-use std::{fs::File, io::BufReader, path::PathBuf};
+use std::{fs::File, path::PathBuf};
 
-use memmap2::{Advice, Mmap};
+use memmap2::Mmap;
+use snafu::{ResultExt, Snafu};
 
-use crate::Result;
+#[derive(Debug, Snafu)]
+pub struct Error(error::Error);
+type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Default)]
 pub struct Document {
@@ -20,11 +23,19 @@ impl Document {
     }
 
     pub fn read(&mut self) -> Result<()> {
-        let file = File::open(&self.path)?;
+        let file = File::open(&self.path).with_context(|_| error::OpenFileSnafu {
+            path: self.path.clone(),
+        })?;
 
-        self.size = file.metadata()?.len();
+        let metadata = file.metadata().with_context(|_| error::MetadataSnafu {
+            path: self.path.clone(),
+        })?;
 
-        let file = unsafe { Mmap::map(&file)? };
+        self.size = metadata.len();
+
+        let _file = unsafe { Mmap::map(&file) }.with_context(|_| error::MmapSnafu {
+            path: self.path.clone(),
+        })?;
 
         // #[cfg(unix)]
         // {
@@ -32,5 +43,33 @@ impl Document {
         // }
 
         Ok(())
+    }
+}
+
+mod error {
+    use std::path::PathBuf;
+
+    use snafu::Snafu;
+
+    #[derive(Debug, Snafu)]
+    #[snafu(visibility(pub(super)))]
+    pub(super) enum Error {
+        #[snafu(display("Failed to open file: {}", path.display()))]
+        OpenFile {
+            path: PathBuf,
+            source: std::io::Error,
+        },
+
+        #[snafu(display("Failed to get metadata for file: {}", path.display()))]
+        Metadata {
+            path: PathBuf,
+            source: std::io::Error,
+        },
+
+        #[snafu(display("Failed to create mmap for file: {}", path.display()))]
+        Mmap {
+            path: PathBuf,
+            source: std::io::Error,
+        },
     }
 }
