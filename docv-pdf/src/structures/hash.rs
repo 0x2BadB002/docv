032b002b410281da97d9ca2c97cb5add582812ad
Kovalev Pavel <kovalev5690@gmail.com>
2025-08-26 15:33:51 +0300

    fix(xref, hash): convert object to hash

--- docv-pdf/src/structures/hash.rs
+++ docv-pdf/src/structures/hash.rs
@@ -1,3 +1,11 @@
+use snafu::{ResultExt, Snafu};
+
+use crate::types::Object;
+
+#[derive(Debug, Snafu)]
+pub struct Error(error::Error);
+type Result<T> = std::result::Result<T, Error>;
+
 #[derive(Debug, Clone)]
 #[allow(dead_code)]
 pub struct Hash {
@@ -6,7 +14,42 @@ pub struct Hash {
 }
 
 impl Hash {
-    pub fn from_data(initial: Vec<u8>, current: Vec<u8>) -> Self {
-        Self { initial, current }
+    pub fn from_object(object: &Object) -> Result<Self> {
+        let array = object.as_array().context(error::InvalidObjectSnafu)?;
+
+        if array.len() != 2 {
+            return Err(error::Error::InvalidArraySize {
+                expected: 2,
+                got: array.len(),
+            }
+            .into());
+        }
+
+        let initial = array[0]
+            .as_string()
+            .context(error::InvalidObjectSnafu)?
+            .as_bytes()
+            .to_vec();
+        let current = array[1]
+            .as_string()
+            .context(error::InvalidObjectSnafu)?
+            .as_bytes()
+            .to_vec();
+
+        Ok(Self { initial, current })
+    }
+}
+
+mod error {
+    use snafu::Snafu;
+
+    #[derive(Debug, Snafu)]
+    #[snafu(visibility(pub(super)))]
+    pub(super) enum Error {
+        #[snafu(display("Object conversion error"))]
+        InvalidObject { source: crate::types::ObjectError },
+
+        #[snafu(display("Wrong array size. Expected = {expected}; Got = {got}"))]
+        InvalidArraySize { expected: usize, got: usize },
     }
 }
--- docv-pdf/src/structures/mod.rs
+++ docv-pdf/src/structures/mod.rs
@@ -6,5 +6,6 @@ pub use hash::Hash;
 pub use info::{Info, Trap};
 pub use xref::{Xref, XrefEntry, XrefMetadata};
 
+pub use hash::Error as HashError;
 pub use info::Error as InfoError;
 pub use xref::Error as XrefError;
